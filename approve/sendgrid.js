2747326468294935e445fdfe2288aa674a976384
Alex Kalderimis <alex.kalderimis@gmail.com>
2019-09-11 22:22:51 +0100

    Fix config bug

--- approve/sendgrid.js
+++ approve/sendgrid.js
@@ -17,7 +17,7 @@ function sendMessageToApprover(invitation) {
   // const content = new sendgrid.mail.Content('text/html', html);
   const mail = {
     to: { email: config.email.from },
-    from: { email: config.email.approver },
+    from: { email: config.approver },
     subject, text, html,
   };
   // const mail = new sendgrid.mail.Mail(from_email, subject, to_email, content);
