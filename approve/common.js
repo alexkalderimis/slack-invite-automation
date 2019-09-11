9e29c416762c98f2cb3e9aeab68bde089e7fd9d2
Alex Kalderimis <alex.kalderimis@gmail.com>
2019-09-11 22:09:29 +0100

    Use @sendgrid/mail

--- approve/common.js
+++ approve/common.js
@@ -1,5 +1,22 @@
 const config = require('../config');
 
+function getText(invitation) {
+  const text = `
+    New request to join ${config.community}
+    =========================================
+
+    We have received a new request from ${invitation.emailAddress}
+    to join ${config.community} on Slack.  If you would like to
+    approve this request, pease click on the link below:
+
+    ${approvalLink(invitation)}
+
+    Your friendly slack bot!
+  `;
+
+  return text;
+}
+
 function getBody(invitation) {
   const html = `
     <h1>New request to join ${config.community}</h1>
@@ -21,4 +38,4 @@ function approvalLink(invitation) {
   return `${config.baseURL}/approve/${invitation.token}`;
 }
 
-module.exports = { approvalLink, getBody };
+module.exports = { approvalLink, getText, getBody };
--- approve/sendgrid.js
+++ approve/sendgrid.js
@@ -1,34 +1,26 @@
 const config = require('../config')
-const sendgrid = require('sendgrid');
 
-const {getBody} = require('./common');
+const sgMail = require('@sendgrid/mail');
 
-const from_email = new sendgrid.mail.Email(config.email.from);
-const to_email = new sendgrid.mail.Email(config.email.approver);
+const {getText, getBody} = require('./common');
+
+// const from_email = new sendgrid.mail.Email(config.email.from);
+// const to_email = new sendgrid.mail.Email(config.email.approver);
 const subject = `A new user wants to join ${config.community}`;
 
-const sg = sendgrid(config.email.sendgrid_api_key);
+sgMail.setApiKey(config.email.sendgrid_api_key);
 
 function sendMessageToApprover(invitation) {
-  const html = getBody(invitation)
-  const content = new sendgrid.mail.Content('text/html', html);
-  const mail = new sendgrid.mail.Mail(from_email, subject, to_email, content);
-
-  const request = sg.emptyRequest({
-    method: 'POST',
-    path: '/v3/mail/send',
-    body: mail.toJSON(),
-  });
-
-  return new Promise((resolve, reject) => {
-    sg.API(request, function(error, response) {
-      if (error) {
-        return reject(error)
-      } else {
-        return resolve(response)
-      }
-    });
-  });
+  const html = getBody(invitation);
+  const text = getText(invitation);
+  // const content = new sendgrid.mail.Content('text/html', html);
+  const mail = {
+    to: { email: config.email.from },
+    from: { email: config.email.approver },
+    subject, text, html,
+  };
+  // const mail = new sendgrid.mail.Mail(from_email, subject, to_email, content);
+  return sgMail.send(mail);
 };
 
 module.exports = { sendMessageToApprover };
