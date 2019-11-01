516626131a405dfe1dd7ea3e38369e1ab792a4d2
Alex Kalderimis <alex.kalderimis@gmail.com>
2019-11-01 16:08:00 +0000

    Ensure all slack functionality is in one place

--- approve/slack.js
+++ approve/slack.js
@@ -1,38 +1,13 @@
 const config = require('../config');
-const request = require('request');
+const Slack = require('../lib/slack');
 
 const { approvalLink } = require('./common');
 
-function sendMessageToApprover(invitation) {
-  const options = {
-    // requires: chat:write:bot
-    url: `https://${config.slackUrl}/api/chat.postMessage`,
-    form: {
-      channel: config.approval.slackChannel,
-      text: `${invitation.emailAddress} wants to join ${config.community}`,
-      attachments: [
-          {
-            text: `Click here to approve them: ${approvalLink(invitation)}`
-          }
-      ]
-      token: config.slacktoken
-    }
-  };
-  return new Promise((resolve, reject) => {
-    request.post(options, function (err, _httpResponse, body) {
-      if (err) return reject(err);
+module.exports = {
+  sendMessageToApprover: invitation => {
+    const text = `${invitation.emailAddress} wants to join ${config.community}`;
+    const attachment = `Click here to approve them: ${approvalLink(invitation)}`;
 
-      let data = null;
-      try {
-        let data = JSON.parse(body);
-      } catch (e) {
-        return reject(e);
-      }
-      if (data.ok) return resolve();
-
-      return reject(new Error(data.error));
-    });
-  });
+    return Slack.sendMessageToChannel(text, [attachment], config.approval.slackChannel);
+  }
 }
-
-module.exports = { sendMessageToApprover };
